09862ae6d5d9c320cc636e3ea63c69197ecdb2d3
Jhonatan Latorre Sierra <61359329+Jhonatanls@users.noreply.github.com>
2024-10-13 17:56:32 -0500

    Prototipo funcional a espera de solucionar errores del carrito de compras e implementación de ordenes de compra

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,69 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+
+from carts.views import _cart_id
+from carts.models import Cart, CartItem
+import requests
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+            user.phone_number = phone_number
+            user.is_active=True
+            user.save()
+            messages.success(request, 'Registro exitoso')
+            return redirect('dashboard')
+    else:
+        form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None:
+            try:
+                print('Entering')
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                print(is_cart_item_exists)
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    print(cart_item)
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+                
+            except:
+                pass
+            auth.login(request, user)
+            #messages.success(request, 'You are now logged in.')
+            return redirect('home')
+            
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Saliste')
+    return redirect('login')
